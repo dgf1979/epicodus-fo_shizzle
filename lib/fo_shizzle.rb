90b3aaab80ff42b8234093f673119d55717b689c
Drew & Peter <student@epicodus.com>
2015-04-20 13:13:03 -0700

    Retroactively added plain english spec file (oops)

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,14 +1,30 @@
+#class String
+#  define_method(:fo_shizzle) do
+#    new_word = ""
+#    letters = self.split("")
+#    letters.each do |letter|
+#      if letter == "s"
+#        new_word += "z"
+#      else
+#        new_word += letter
+#      end
+#    end
+#    new_word
+#  end
+#end
+
 class String
   define_method(:fo_shizzle) do
-    new_word = ""
+    new_word = []
     letters = self.split("")
     letters.each do |letter|
       if letter == "s"
-        new_word += "z"
+        new_word.push("z")
       else
-        new_word += letter
+        new_word.push("letter")
       end
     end
+    new_word.join("").to_s
     new_word
   end
 end
